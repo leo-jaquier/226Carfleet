44686666933997f04e3182b28da03d33dd4ebf15
leo-jaquier <leo.jaquier@eduvaud.ch>
2023-11-15 11:42:36 +0100

    print info console

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,20 +1,36 @@
-﻿namespace Carfleet
+﻿using System.Diagnostics;
+
+namespace Carfleet
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //TODO
+            #region private attributes
+            string _registration = "VD 123 567";
+            string _brand = "Mercedes-Benz";
+            string _model = "Vito";
+            string _chassisNumber = "SV30-0169266";
+            Car _car;
+            Truck _truck;
+            _car = new Car("VD 123 567", "Mercedes-Benz", "Vito", "SV42-6666666");
+            _truck = new Truck("FR 123 567","Renaud", "Vita", "SV99-9696969");
+            #endregion private attributes
+
+
+
+            DisplayCar(_car);
+            DisplayTruck(_truck);
         }
 
         static private void DisplayCar(Car car)
         {
-            //TODO
+            Console.WriteLine(car.ToString());
         }
 
         static private void DisplayTruck(Truck truck)
         {
-            //TODO
+            Console.WriteLine(truck.ToString());
         }
     }
 }
